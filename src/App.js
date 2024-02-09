4a8786964c2f554d051dad575a5e68f8cac4b0ec
estudart <erico.studartdf@gmail.com>
2024-02-09 18:14:09 -0300

    home page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import NavBar from "./components/NavBar.js";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Copa do Mundo 2002</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      import sample from './sample.mp4';
-      <YoutubeEmbed embedId="uED_3a38DPg" />
-    </div>
+    <>
+      <NavBar />
+      <div className="App">
+        <h1 className="home-page-title">Copa do Mundo 2002</h1>
+        <div className="home-page-container_1">
+          <img
+            className="home-page-container_1--icon"
+            src="https://i.pinimg.com/originals/6a/4d/77/6a4d77381edc99d9290e1a3073e3b5cd.png"
+            alt="Brazil Icon"
+          />
+          <p className="home-page-container_1--text">
+            A Copa do Mundo de 2002 no Japão e na Coreia do Sul foi repleta de
+            momentos emocionantes e surpresas. Com estrelas como Ronaldo e
+            Ronaldinho brilhando, o torneio cativou o mundo com sua paixão pelo
+            futebol. A vitória do Brasil sobre a Alemanha na final, e a
+            surpreendente performance da seleção sul-coreana, foram alguns dos
+            destaques deste evento histórico. A Copa de 2002 deixou um legado
+            duradouro, demonstrando a capacidade da Ásia em sediar eventos
+            esportivos de grande porte e inspirando uma nova geração de fãs e
+            jogadores de futebol.
+          </p>
+        </div>
+        <YoutubeEmbed embedId="uED_3a38DPg" />
+      </div>
+    </>
   );
 }
 
@@ -16,8 +36,8 @@ function YoutubeEmbed({ embedId }) {
   return (
     <div className="video-responsive">
       <iframe
-        width="853"
-        height="480"
+        width="425"
+        height="240"
         src={`https://www.youtube.com/embed/${embedId}`}
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
---
+++ src/components/NavBar.js
@@ -0,0 +1,59 @@
+export default function NavBar() {
+  return (
+    <nav
+      className="navbar navbar-expand-md navbar-dark"
+      style={{
+        backgroundColor: "black",
+        border: "1px solid white",
+        borderTop: "0px",
+      }}
+    >
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          Fixed navbar
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarCollapse"
+          aria-controls="navbarCollapse"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarCollapse">
+          <ul className="navbar-nav me-auto mb-2 mb-md-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Link
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link disabled" aria-disabled="true">
+                Disabled
+              </a>
+            </li>
+          </ul>
+          <form className="d-flex" role="search">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </div>
+    </nav>
+  );
+}
